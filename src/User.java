f42917babab51bdacc5284ff32ebf5a548bf2a84
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-03-07 23:40:27 +0530

    initial files

---
+++ src/User.java
@@ -0,0 +1,8 @@
+public class User {
+    /*Users of the game need to create user profiles, providing a name and a username. The
+    name can be changed at any time, but the username cannot be changed once created. The
+    username is a unique value; no two players can have the same username. If an attempt is
+    made to create a new player profile with an existing username, the user should be notified
+    that the username is already taken and should be prompted to enter a new username. Each
+    user should also have a unique integer userID, which should be given by the system.*/
+}
