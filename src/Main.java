7f6a72d4484e5080bb91a7a0267aa77c1f96cdb9
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-03-08 09:09:12 +0530

    Main file completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+
+        Game game = new Game();
+
+        String[] army = {"Ranger", "Squire", "Warlock", "Medic", "Dragon"};
+        String[] equipment = {"Chainmail", null, null, "Amulet" , null};
+        Player whitewolf = game.existingPlayer("GeraltofRivia", "whitewolf", 32, 215, "Marshland", army, equipment)
+
+        Player jacob = game.newPlayer("Jacob Lara", "jacob")
+
+        jacob.buy("Archers", "Shooter");
+        jacob.buy("Knights", "Squire");
+        jacob.buy("Mages", "Warlock");
+        jacob.buy("Healers", "Soother");
+        jacob.buy("MythicalCreatures", "Dragon");
+
+        jacob.sell("Archers");
+        jacob.buy("Archers", "Ranger");
+
+        game.challenge(alice, jason);
+
+        */
     }
 }
