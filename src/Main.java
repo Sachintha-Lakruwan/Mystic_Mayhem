df571c6486a0ac6b4107c959f97febcf51a85f34
NesanduGMS <sithnukagunarathna@gmail.com>
2024-03-09 17:29:37 +0530

    Start game

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        //initialize the existing player and his army
+        // initialize the existing player and his army
         Player whiteWolf = new Player("GeraltofRivia", "whitewolf", 215, 32);
-        String[] troops = {"Ranger", "Squire", "Warlock", "Medic", "Dragon"};
+        String[] troops = { "Ranger", "Squire", "Warlock", "Medic", "Dragon" };
         whiteWolf.createArmy(troops);
         whiteWolf.army.archer.setEquipment("Chainmail");
         whiteWolf.army.healer.setEquipment("Amulet");
 
-        //register new player
+        // register new player
         Player jacob = new Player("Jacob Lara", "jacob");
 
-        //now, two players are created successfully. one of them is whitewolf and his army is already created.
-        // other one is Jacob, who was created by the user, and his army is yet to be created
-
-
+        // now, two players are created successfully. one of them is whitewolf and his
+        // army is already created.
+        // other one is Jacob, who was created by the user, and his army is yet to be
+        // created
 
         whiteWolf.buyEquipment("Knight", "Chainmail");
         System.out.println(whiteWolf.army.knight.attack);
+        System.out.println("Start Game");
     }
 }
