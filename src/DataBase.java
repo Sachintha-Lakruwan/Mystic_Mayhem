ecbf3a107bce6fc0256d0e85610c844009317f51
Sachion123 <sachi.new2002@gmail.com>
2024-03-13 01:29:56 +0530

    db changed

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class DataBase {
 
@@ -11,7 +10,7 @@ public class DataBase {
     // Constructor to initialize the equipment list with data
     public DataBase() {
         equipmentList = new ArrayList<>();
-        soldiersData = new ArrayList<>();
+        soldiersData=new ArrayList<>();
 
         // Add data to the equipment list
         equipmentList.add(Arrays.asList("Chainmail", 70, 0, 1, 0, -1));
@@ -23,27 +22,35 @@ public class DataBase {
 
         // Add data to the soldier list
 
-        soldiersData.add(Arrays.asList("Shooter", 80, 11, 4, 6, 9));
-        soldiersData.add(Arrays.asList("Squire", 85, 8, 9, 7, 8));
-        soldiersData.add(Arrays.asList("Warlock", 100, 12, 7, 10, 12));
-        soldiersData.add(Arrays.asList("Soother", 95, 10, 8, 9, 6));
-        soldiersData.add(Arrays.asList("Dragon", 120, 12, 14, 15, 8));
-        soldiersData.add(Arrays.asList("Ranger", 115, 14, 5, 8, 10));
-        soldiersData.add(Arrays.asList("Cavalier", 110, 10, 12, 7, 10));
-        soldiersData.add(Arrays.asList("Illusionist", 120, 13, 8, 12, 14));
-        soldiersData.add(Arrays.asList("Medic", 125, 12, 9, 10, 7));
-        soldiersData.add(Arrays.asList("Basilisk", 165, 15, 11, 10, 12));
-        soldiersData.add(Arrays.asList("Sunfire", 160, 15, 5, 7, 14));
-        soldiersData.add(Arrays.asList("Templar", 155, 14, 16, 12, 12));
-        soldiersData.add(Arrays.asList("Enchanter", 160, 16, 10, 13, 16));
-        soldiersData.add(Arrays.asList("Alchemist", 150, 13, 13, 13, 13));
-        soldiersData.add(Arrays.asList("Hydra", 205, 12, 16, 15, 11));
-        soldiersData.add(Arrays.asList("Zing", 200, 16, 9, 11, 14));
-        soldiersData.add(Arrays.asList("Zoro", 180, 17, 16, 13, 14));
-        soldiersData.add(Arrays.asList("Conjurer", 195, 18, 15, 14, 12));
-        soldiersData.add(Arrays.asList("Saint", 200, 16, 14, 17, 9));
-        soldiersData.add(Arrays.asList("Phoenix", 275, 17, 13, 17, 19));
+        soldiersData.add(Arrays.asList("Shooter", "Archer", 80, 11, 4, 6, 9));
+        soldiersData.add(Arrays.asList("Squire", "Knight", 85, 8, 9, 7, 8));
+        soldiersData.add(Arrays.asList("Warlock", "Mage", 100, 12, 7, 10, 12));
+        soldiersData.add(Arrays.asList("Soother", "Healer", 95, 10, 8, 9, 6));
+        soldiersData.add(Arrays.asList("Dragon", "Mythical", 120, 12, 14, 15, 8));
+        soldiersData.add(Arrays.asList("Ranger", "Archer", 115, 14, 5, 8, 10));
+        soldiersData.add(Arrays.asList("Cavalier", "Knight", 110, 10, 12, 7, 10));
+        soldiersData.add(Arrays.asList("Illusionist", "Mage", 120, 13, 8, 12, 14));
+        soldiersData.add(Arrays.asList("Medic", "Healer", 125 , 12, 9, 10, 7));
+        soldiersData.add(Arrays.asList("Basilisk", "Mythical", 165, 15, 11, 10, 12));
+        soldiersData.add(Arrays.asList("Sunfire", "Archer", 160, 15, 5, 7, 14));
+        soldiersData.add(Arrays.asList("Templar", "Knight", 155, 14, 16, 12, 12));
+        soldiersData.add(Arrays.asList("Enchanter", "Mage", 160, 16, 10, 13, 16));
+        soldiersData.add(Arrays.asList("Alchemist", "Healer", 150, 13, 13, 13, 13));
+        soldiersData.add(Arrays.asList("Hydra", "Mythical", 205, 12, 16, 15, 11));
+        soldiersData.add(Arrays.asList("Zing", "Archer", 200, 16, 9, 11, 14));
+        soldiersData.add(Arrays.asList("Zoro", "Knight", 180, 17, 16, 13, 14));
+        soldiersData.add(Arrays.asList("Conjurer", "Mage", 195, 18, 15, 14, 12));
+        soldiersData.add(Arrays.asList("Saint", "Healer", 200, 16, 14, 17, 9));
+        soldiersData.add(Arrays.asList("Phoenix", "Mythical", 275, 17, 13, 17, 19));
+        soldiersData.add(Arrays.asList("Saggitarius", "Archer", 230, 18, 7, 12, 17));
+        soldiersData.add(Arrays.asList("Swiftblade", "Knight", 250, 18, 20, 17, 13));
+        soldiersData.add(Arrays.asList("Eldritch", "Mage", 270, 19, 17, 18, 14));
+        soldiersData.add(Arrays.asList("Lightbringer", "Healer   ", 260, 17, 15, 19, 12));
+        soldiersData.add(Arrays.asList("Pegasus", "Mythical", 340, 14, 18, 20, 20));
     }
+
+
+
 
     // Method to get soldier data by name
     public int[] getEquipmentData(String name) {
@@ -54,27 +61,31 @@ public class DataBase {
                 int defence = (int) soldier.get(3);
                 int health = (int) soldier.get(4);
                 int speed = (int) soldier.get(5);
-                int[] data = { price, attack, defence, health, speed };
+
+                int[] data = {price, attack, defence, health, speed};
                 return data;
             }
         }
         return null; // Return null if no match found
     }
-
-    public int[] getSoldierData(String name) {
+    public Object[] getSoldierData(String name) {
         for (List<Object> soldier : soldiersData) {
 
             if (name.equals(soldier.get(0))) {
-                int price = (int) soldier.get(1);
-                int attack = (int) soldier.get(2);
-                int defence = (int) soldier.get(3);
-                int health = (int) soldier.get(4);
-                int speed = (int) soldier.get(5);
-                int[] data = { price, attack, defence, health, speed };
+                int price = (int) soldier.get(2);
+                int attack = (int) soldier.get(3);
+                int defence = (int) soldier.get(4);
+                int health = (int) soldier.get(5);
+                int speed = (int) soldier.get(6);
+                String category=(String) soldier.get(1);
+                Object[] data = {price, attack, defence, health, speed,category};
                 return data;
             }
         }
         return null; // Return null if no match found
     }
+
+
+
 
 }
